8c321447abfed7a4993f224f57fe87c9eb9f5f5a
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-10-04 20:33:25 +0300

    Sweet changes

--- README.md
+++
@@ -1,5 +0,0 @@
-# Improved Shadow Remover Tool 🛠
-Improved version of the original shadow remover tool
-
-## Contributing 💻: 
-Try to adhere to Wiremod's [Developer Style Guide](https://github.com/wiremod/wire/wiki/Developer-Style-Guide)
--- lua/weapons/gmod_tool/stools/shadowremover.lua
+++ lua/weapons/gmod_tool/stools/shadowremover.lua
@@ -5,14 +5,24 @@ TOOL.Information = {
 	{name = "right"}
 }
 
+TOOL.ClientConVar["fullbright"] = "1"
+TOOL.ClientConVar["disableshadow"] = "1"
+
 local function MakeShadowRemoved(ply, ent, data)
 	-- Double check to prevent exploits
-	if ent:GetClass() == "prop_physics" then
+	local class = ent:GetClass()
+
+	if class == "prop_physics" or class == "prop_ragdoll" then
 		if data.Shadow then
 			ent:SetNW2Bool("ShadowRemover", true)
 			duplicator.StoreEntityModifier(ent, "ShadowRemover", data)
 		else
 			ent:SetNW2Bool("ShadowRemover", nil)
+		end
+
+		ent:DrawShadow(not data.DisableShadow)
+
+		if not data.DisableShadow and not data.Shadow then
 			duplicator.ClearEntityModifier(ent, "ShadowRemover")
 		end
 	end
@@ -23,8 +33,8 @@ if SERVER then
 else
 	language.Add("tool.shadowremover.name", "Shadow Remover")
 	language.Add("tool.shadowremover.desc", "Disables object shadows")
-	language.Add("tool.shadowremover.left", "Remove shadows")
-	language.Add("tool.shadowremover.right", "Add shadows")
+	language.Add("tool.shadowremover.left", "Add shadows")
+	language.Add("tool.shadowremover.right", "Remove shadows")
 
 	hook.Add("EntityNetworkedVarChanged", "ShadowRemover", function(ent, name, old, new)
 		if name == "ShadowRemover" then
@@ -39,32 +49,46 @@ else
 			end
 		end
 	end)
+
+	function TOOL.BuildCPanel(panel)
+		panel:Help("Removes the shadow of an object or makes it fullbright")
+		panel:CheckBox("Make object fullbright", "shadowremover_fullbright")
+		panel:CheckBox("Disable object shadow", "shadowremover_disableshadow")
+	end
 end
 
 function TOOL:LeftClick(trace)
-    local ent = trace.Entity
+	local ent = trace.Entity
 
-    if IsValid(ent) and ent:GetClass() == "prop_physics" then
-        if CLIENT then return true end
+	if IsValid(ent) then
+		local class = ent:GetClass()
 
-        MakeShadowRemoved(self:GetOwner(), ent, {Shadow = true})
+		if class == "prop_physics" or class == "prop_ragdoll" then
+			if CLIENT then return true end
 
-        return true
-    end
+			MakeShadowRemoved(self:GetOwner(), ent, {Shadow = self:GetClientBool("fullbright"), DisableShadow = self:GetClientBool("disableshadow")})
 
-    return false
+			return true
+		end
+	end
+
+	return false
 end
 
 function TOOL:RightClick(trace)
-    local ent = trace.Entity
+	local ent = trace.Entity
 
-    if IsValid(ent) and ent:GetClass() == "prop_physics" then
-        if CLIENT then return true end
+	if IsValid(ent) then
+		local class = ent:GetClass()
 
-        MakeShadowRemoved(self:GetOwner(), ent, {Shadow = false})
+		if class == "prop_physics" or class == "prop_ragdoll" then
+			if CLIENT then return true end
 
-        return true
-    end
+			MakeShadowRemoved(self:GetOwner(), ent, {Shadow = false, DisableShadow = false})
 
-    return false
+			return true
+		end
+	end
+
+	return false
 end
